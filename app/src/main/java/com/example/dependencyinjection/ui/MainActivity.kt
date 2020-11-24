c179298d0820fa6b54da5800e84db7e8a655bf4a
Renaldy <renaldy@192.168.1.7>
2020-11-24 23:54:56 +0700

    [add] adding gradle for project

--- app/src/main/java/com/example/dependencyinjection/ui/MainActivity.kt
+++ app/src/main/java/com/example/dependencyinjection/ui/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.dependencyinjection
+package com.example.dependencyinjection.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.dependencyinjection.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
