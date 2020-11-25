bf319db59c8c25347189752b89087d64c3be4b46
Renaldy <renaldy@192.168.1.7>
2020-11-25 22:51:25 +0700

    [add] adding api for fetch data

--- app/src/main/java/com/example/dependencyinjection/data/service/ApiService.kt
+++ app/src/main/java/com/example/dependencyinjection/data/service/ApiService.kt
@@ -1,2 +1,9 @@
 package com.example.dependencyinjection.data.service
 
+import retrofit2.http.GET
+
+interface ApiService {
+
+    @GET("api/v1/json/1/all_sports.php")
+    fun getAllSport()
+}
--- app/src/main/java/com/example/dependencyinjection/utils/StaticString.kt
+++ app/src/main/java/com/example/dependencyinjection/utils/StaticString.kt
@@ -1,2 +1,3 @@
 package com.example.dependencyinjection.utils
 
+const val BASE_URl = "https://www.thesportsdb.com/"
